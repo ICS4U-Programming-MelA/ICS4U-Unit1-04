a72c73ed273e2415d834ebb150cd740dfd6b88ad
Ubuntu <ubuntu@ip-172-31-16-149.ec2.internal>
2021-12-06 17:18:22 +0000

    Added proper comments to DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -1,26 +1,33 @@
 /* Generates a random number between 1 and 6 which the user has to guess for. If the guess is
  * wrong, it displays as such, and when finished it will tell them how many tries it took.
  */
-// Module imports.
+// Import modules..
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-// Contains and runs the dice game described above.
 class DiceGame {
   public static void main(String[] args) {
+
+    // Declare varaibles.
     int counter = 0;
     int userNumber = 0;
     Random random = new Random();
     int randomInt = random.nextInt(7);
+
+    // Introduce the program.
     System.out.print("This program has a random number between 1 and 6 which you have to guess for."
                    + " It will keep going until you get the right number, and upon guessing"
                    + " correctly, displays how many tries you took.");
     Scanner input = new Scanner(System.in);
     do {
       try {
+
+        // Get the user's number.
         System.out.print("\n" + "Enter a number between 1 and 6: ");
         userNumber = input.nextInt();
+
+        // Check and dislpay if the user's number is correct or incorrect.
         if (userNumber < 1 || userNumber > 6) {
           System.out.print("\n" + userNumber + " is out of range. Please try again.");
         } else if (userNumber == randomInt) {
@@ -35,7 +42,11 @@ class DiceGame {
             System.out.print("\n" + "You guessed too low. Please try again.");
           }
         }
+
+        // Increcment the counter.
         counter = counter + 1;
+
+        // Catch and display when the user's number is invalid.
       } catch (InputMismatchException e1) {
         System.out.print("\n" + "That isn't an integer. Please try again.");
         counter = counter + 1;
